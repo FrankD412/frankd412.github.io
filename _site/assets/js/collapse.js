8bd8a9cbecdcffb08416b85e0648f45160b2777d
Francesco Di Natale <dinatale3@llnl.gov>
2020-01-19 22:07:38 -0800

    Update chevrons 01192020

--- _site/assets/js/collapse.js
+++ _site/assets/js/collapse.js
@@ -11,15 +11,18 @@ $("a.collapsible").click(function () {
 
 $("a.collapsible").click(function () {
     $header = $(this);
-    var id = event.target.id;
-    var value = event.target.value;
-    console.log(id);
-    var name = id + "-" + value;
+    var key = event.target.getAttribute("key");
+    var value = event.target.getAttribute("content");
+    console.log(key);
+    var name = key + "-" + value;
     console.log(name);
 
-    
+    $chevron = $('#' + name + "-chev")
+    console.log(name + "-chev")
+    $chevron.toggleClass("down")
     //getting the next element
     $content = $('#' + name);
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () { });
 });
+
