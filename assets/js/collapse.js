a86a4d8202bedccf3a121a2c2c5df6e0edbbbeeb
Francesco Di Natale <dinatale3@llnl.gov>
2020-01-19 22:03:59 -0800

    Addition of rotating chevron.

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -11,13 +11,15 @@ $("a.collapsible").click(function () {
 
 $("a.collapsible").click(function () {
     $header = $(this);
-    var id = event.target.id;
+    var key = event.target.getAttribute("key");
     var value = event.target.getAttribute("content");
-    console.log(id);
-    var name = id + "-" + value;
+    console.log(key);
+    var name = key + "-" + value;
     console.log(name);
 
-    
+    $chevron = $('#' + name + "-chev")
+    console.log(name + "-chev")
+    $chevron.toggleClass("down")
     //getting the next element
     $content = $('#' + name);
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
